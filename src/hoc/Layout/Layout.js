e7bd878fb566d81bd01742ac3565b63b937a8f2e
Vadim <vadim.prog.2000@gmail.com>
2020-10-23 01:48:07 +0300

    create navigation and burger menu

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
 import classes from './Layout.module.css'
 
 class Layout extends Component {
+	state = {
+		menu: false
+	}
+
+	toggleMenuHandler = () => {
+		this.setState({
+			menu: !this.state.menu
+		})
+	}
+
 	render() {
 		return (
 			<div className={classes.Layout}>
 
+				<Drawer
+					isOpen={this.state.menu}
+					onClose={this.toggleMenuHandler}
+				/>
+
+				<MenuToggle
+					onToggle={this.toggleMenuHandler}
+					isOpen={this.state.menu}
+				/>
 
 				<main>
 					{this.props.children}
