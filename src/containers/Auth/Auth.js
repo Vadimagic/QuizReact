b1096d96746c43f3819b407c18564659549a7df7
Vadim <vadim.prog.2000@gmail.com>
2020-10-24 02:28:24 +0300

    create inputs state and renderInputs()

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,36 @@ import classes from './Auth.module.css'
 
 export default class Auth extends Component {
 
+	state = {
+		formControls : {
+			email: {
+				value: '',
+				type: 'email',
+				label: 'Email',
+				errorMessage: 'Введите корректный email',
+				valid: false,
+				touched: false,
+				validation: {
+					require: true,
+					email: true
+				}
+			},
+			password: {
+				value: '',
+				type: 'password',
+				label: 'Пароль',
+				errorMessage: 'Введите корректный пароль',
+				valid: false,
+				touched: false,
+				validation: {
+					require: true,
+					minLength: 6,
+					maxLength: 16
+				}
+			}
+		}
+	}
+
 	loginHandler = () => {
 
 	}
@@ -17,6 +47,32 @@ export default class Auth extends Component {
 		e.preventDefault()
 	}
 
+	onChangeHandler = (event, controlName) => {
+		console.log(`${controlName}: ${event.target.value} : ${typeof(event.target.value)} : ${event.target.value[event.target.value.length - 1]}`)
+		const stateCopy = this.state;
+		stateCopy.formControls[controlName].value = event.target.value;
+		this.setState(stateCopy)
+	}
+
+	renderInputs() {
+		return Object.keys(this.state.formControls).map((controlName, index) => {
+			const control = this.state.formControls[controlName]
+			return (
+				<Input 
+					key={controlName + index}
+					type={control.type}
+					label={control.label}
+					value={control.value}
+					errorMessage={control.errorMessage}
+					valid={control.valid}
+					touched={control.touched}
+					shouldValidate={control.validation.require}
+					onChange={(event) => this.onChangeHandler(event, controlName)}
+				/>
+			)
+		})
+	}
+
 	render() {
 		return (
 			<div className={classes.Auth}>
@@ -24,13 +80,7 @@ export default class Auth extends Component {
 					<h1>Авторизация</h1>
 
 					<form onSubmit={this.submitHandler} className={classes.AuthForm}>
-						<Input 
-							label="Email"
-							// errorMessage="TEST"
-						/>
-						<Input 
-							label="Пароль"
-						/>
+						{this.renderInputs()}
 						<Button 
 							type="success" 
 							onClick={this.loginHandler}
